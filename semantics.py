a27569c5c30a35d0fd2ebea8d826e052b2eea9cc
Muhammad Ibraheem <73916528+MoRanderee@users.noreply.github.com>
2024-03-31 14:24:01 +0200

    Updated .py file

--- semantics.py
+++ semantics.py
@@ -5,6 +5,7 @@ word1 = nlp("cat")
 word2 = nlp("monkey")
 word3 = nlp("banana")
 
+print("\nWord comparison:\n")
 print(word1.similarity(word2))
 print(word3.similarity(word2))
 print(word3.similarity(word1))
@@ -19,6 +20,7 @@ for token1 in tokens:
     for token2 in tokens:
         print(token1.text, token2.text, token1.similarity(token2))
 
+print("\nSentence comparison:\n")
 sentence_to_compare = "Why is my cat on the car"
 
 sentences = ["where did my dog go",
